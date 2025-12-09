5089b737b4a8448cfd76685a731b14e1b237dfd9
chaya2350 <cha182350@gmail.com>
2025-12-09 16:00:33 +0200

    without excelium

--- lab03_uvc/tb/xcelium.d/run.lnx8664.24.09.d/files.ts
+++
@@ -1,6 +0,0 @@
-1759163469 /project/uvma/users/ayalamu/ws/UVMA_v1_2_5_rev2/uvm/lab01_data/sv/yapp_pkg.sv
-1759163263 /project/uvma/users/ayalamu/ws/UVMA_v1_2_5_rev2/uvm/lab01_data/tb/top.sv
-1759227612 /project/uvma/users/ayalamu/ws/UVMA_v1_2_5_rev2/uvm/lab02_test/sv/yapp_pkg.sv
-1759229521 /project/uvma/users/ayalamu/ws/UVMA_v1_2_5_rev2/uvm/lab02_test/tb/top.sv
-1760522351 /project/uvma/users/ayalamu/ws/UVMA_v1_2_5_rev2/uvm/lab03_uvc/sv/yapp_pkg.sv
-1760516451 /project/uvma/users/ayalamu/ws/UVMA_v1_2_5_rev2/uvm/lab03_uvc/tb/top.sv
